f6a6729da41716f70d63a27e65686c17676240a6
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-06 16:03:56 +0300

    🎉 feat(server): Add CORS headers and endpoint to app
    The commit introduces changes to the server's package.json and app.ts files. Here's a breakdown of the changes:
    
    1. In package.json:
       - Added the "express" dependency to enable using Express.js for the server.
    
    2. In app.ts:
       - Imported the "http" module to create an HTTP server.
       - Imported the "dotenv" and "express" modules.
       - Configured CORS headers to allow requests from any origin.
       - Added a basic endpoint at the root URL that returns "Hello, world!".
       - Configured the server to parse incoming JSON data.
       - Created an HTTP server using the Express app and set it to listen on the specified port.
       - Updated the console log to display the server's URL with a rocket emoji and termination instructions.
    
    Changes made:
    - Added the "express" dependency to package.json.
    - Configured CORS headers to allow requests from any origin.
    - Added a basic endpoint at the root URL.
    - Configured the server to parse JSON data.
    - Updated the console log to display the server's URL with termination instructions.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,29 @@
-import 'dotenv/config';
+import http from 'http';
 
-console.log('Starting ...');
+import 'dotenv/config';
+import express from 'express';
+
+export const app = express();
+export const port = process.env.PORT || '3000';
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.use(express.json());
+
+export const server = http.createServer(app);
+server.listen(port);
+
+console.log(
+  `🚀 Server is running at http://localhost:${port}` +
+    ' ' +
+    'press Ctrl-C to terminate....'
+);
