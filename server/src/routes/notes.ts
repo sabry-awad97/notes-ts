c0d48e605de2bbcf6e68c922a9c4b6a7d249f62e
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-06 18:19:51 +0300

    🔧 refactor(notes.ts): Refactor note routes for better code organization and reusability 🆕 feat(notedestroy.hbs): Add template for note deletion confirmation form 🆕 feat(noteview.hbs): Add template for displaying note details with edit and delete options The note routes in `notes.ts` have been refactored to improve code organization and reusability. The changes include:
    - Moved the import statement for `Note` from `'../models/Notes.js'` to the top of the file for better clarity and consistency.
    - Added a new route `/save` to handle both note creation and update based on the `req.body.docreate` value.
    - Updated the `router.get('/add')` route to render the `noteedit` template with `docreate` set to `true`.
    - Added new routes `/view`, `/edit`, and `/destroy` to render the `noteview`, `noteedit`, and `notedestroy` templates respectively.
    - Added a new route `/destroy/confirm` to handle the actual deletion of a note.
    - Created the `notedestroy.hbs` template for displaying the note deletion confirmation form.
    - Created the `noteview.hbs` template for displaying note details along with options to edit and delete the note.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { NotesStore as notes } from '../app.js';
+import { Note } from '../models/Notes.js';
 export const router = express.Router();
 
 // Add Note. (create)
@@ -10,4 +11,77 @@ router.get('/add', (req, res, next) => {
     notekey: '',
     note: undefined,
   });
+});
+
+// Save Note (update)
+router.post('/save', async (req, res, next) => {
+  try {
+    let note: Note;
+    if (req.body.docreate === 'create') {
+      note = await notes.create(
+        req.body.notekey,
+        req.body.title,
+        req.body.body
+      );
+    } else {
+      note = await notes.update(
+        req.body.notekey,
+        req.body.title,
+        req.body.body
+      );
+    }
+    res.redirect('/notes/view?key=' + note.key);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/view', async (req, res, next) => {
+  try {
+    let note = await notes.read(req.query.key as string);
+    res.render('noteview', {
+      title: note ? note.title : '',
+      notekey: req.query.key,
+      note: note,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/edit', async (req, res, next) => {
+  try {
+    let note = await notes.read(req.query.key as string);
+    res.render('noteedit', {
+      title: note ? 'Edit ' + note.title : 'Add a Note',
+      docreate: false,
+      notekey: req.query.key,
+      note: note,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/destroy', async (req, res, next) => {
+  try {
+    let note = await notes.read(req.query.key as string);
+    res.render('notedestroy', {
+      title: note ? note.title : '',
+      notekey: req.query.key,
+      note: note,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Really destroy note (destroy)
+router.post('/destroy/confirm', async (req, res, next) => {
+  try {
+    await notes.destroy(req.body.notekey);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
 });
