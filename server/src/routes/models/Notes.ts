e9298f9d8fe4a53edd7243c8aa1f5d0d4f3ac880
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-06 17:17:03 +0300

    ✨ feat(Notes.ts): Add Note and AbstractNotesStore classes
    This commit adds the `Note` and `AbstractNotesStore` classes to handle notes functionality in the application.
    
    Changes made:
    - Created the `Note` class with private properties `_note_key`, `_note_title`, and `_note_body`.
    - Added a constructor to initialize the `Note` object with key, title, and body values.
    - Implemented getter and setter methods for `key`, `title`, and `body`.
    - Created the `AbstractNotesStore` abstract class to define the interface for managing notes.
    - Added abstract methods `update`, `create`, `read`, `destroy`, `keyList`, and `count` to be implemented by the concrete notes store classes.
    
    These classes provide the foundation for managing notes and interacting with the notes store.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

---
+++ server/src/routes/models/Notes.ts
@@ -0,0 +1,63 @@
+export class Note {
+  private _note_key: string;
+  private _note_title: string;
+  private _note_body: string;
+
+  constructor(key: string, title: string, body: string) {
+    this._note_key = key;
+    this._note_title = title;
+    this._note_body = body;
+  }
+
+  get key() {
+    return this._note_key;
+  }
+
+  get title() {
+    return this._note_title;
+  }
+
+  set title(newTitle: string) {
+    this._note_title = newTitle;
+  }
+
+  get body() {
+    return this._note_body;
+  }
+
+  set body(newBody: string) {
+    this._note_body = newBody;
+  }
+}
+
+export abstract class AbstractNotesStore {
+  /**
+   * Asynchronous method to update a note
+   */
+  abstract update(key: string, title: string, body: string): Promise<void>;
+
+  /**
+   * Asynchronous method to create a new note
+   */
+  abstract create(key: string, title: string, body: string): Promise<void>;
+
+  /**
+   * Asynchronous method to read a note
+   */
+  abstract read(key: string): Promise<Note>;
+
+  /**
+   * Asynchronous method to delete a note
+   */
+  abstract destroy(key: string): Promise<void>;
+
+  /**
+   * Asynchronous method to retrieve a list of note keys
+   */
+  abstract keyList(): Promise<string[]>;
+
+  /**
+   * Asynchronous method to count the number of notes
+   */
+  abstract count(): Promise<number>;
+}
