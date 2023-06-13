ae97048e01808d259d56caf2419c128864968ae8
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-13 20:31:56 +0300

    🎉 feat(NoteEdit.tsx): Implement note editing functionality 🔧 fix(NoteEdit.tsx): Fix initial note state and updateNote function The `NoteEdit` component now includes the functionality to edit notes. The component is responsible for rendering a form to edit the note's key, title, and body. When the form is submitted, the `updateNote` function is called to update the note on the server.
    Changes made:
    - Imported necessary dependencies: `useEffect`, `useState`, `Header`, `useNavigate`, `useSearchParams`, `updateNote`, and `readNote`.
    - Updated the component's props to an empty object since the `docreate` and `note` props are no longer used.
    - Created a local state for `note` using the `useState` hook, initialized with empty values for `key`, `title`, and `body`.
    - Used the `useSearchParams` hook to retrieve the `key` parameter from the URL.
    - Implemented the `useEffect` hook to fetch the note data from the server when the component mounts. The fetched note is then set in the local state using the `setNote` function.
    - Created event handlers for input and textarea elements to update the `note` state when the user modifies the values.
    - Implemented the `handleSubmit` function to handle form submission. It prevents the default form behavior, calls the `updateNote` function to update the note on the server, and navigates to the view page for the updated note.
    - Rendered the form with input fields for the note's key and title, and a textarea for the note's body. The values are bound to the `note` state, and the event handlers are assigned to the `onChange` event.
    - Added a submit button to the form.
    - Added a `Header` component to display the page title as "Edit [note title]".
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- client/src/pages/NoteEdit.tsx
+++ client/src/pages/NoteEdit.tsx
@@ -1,30 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { INote } from '../types';
+import Header from './Header';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { updateNote, readNote } from '../api/api';
 
-interface Props {
-  docreate: 'create' | 'update';
-  note: INote;
-}
+interface Props {}
 
-const NoteEdit: React.FC<Props> = ({ docreate, note }) => (
-  <form method="POST" action="/notes/save">
-    <input
-      type="hidden"
-      name="docreate"
-      value={docreate ? 'create' : 'update'}
-    />
-    <p>
-      Key: {docreate ? <input type="text" name="notekey" value="" /> : note.key}
-      <input type="hidden" name="notekey" value={note ? note.key : ''} />
-    </p>
-    <p>
-      Title: <input type="text" name="title" value={note ? note.title : ''} />
-    </p>
-    <br />
-    <textarea rows={5} cols={40} name="body" value={note ? note.body : ''} />
-    <br />
-    <input type="submit" value="Submit" />
-  </form>
-);
+const NoteEdit: React.FC<Props> = () => {
+  const [note, setNote] = useState<INote>({
+    key: '',
+    title: '',
+    body: '',
+  });
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const key = searchParams.get('key');
+
+    const getNote = async () => {
+      if (key) {
+        const note = await readNote(key);
+        console.log(note);
+
+        setNote(note);
+      }
+    };
+
+    getNote();
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setNote({ ...note, [name]: value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { key } = note;
+    await updateNote(note);
+    navigate('/notes/view?key=' + key);
+  };
+
+  return (
+    <>
+      <Header title={`Edit ${note?.title}`} />
+      <form method="POST" onSubmit={handleSubmit}>
+        <p>
+          Key:{' '}
+          <input
+            type="text"
+            name="key"
+            value={note.key}
+            onChange={handleInputChange}
+          />
+          <input type="hidden" name="key" value={note ? note.key : ''} />
+        </p>
+        <p>
+          Title:{' '}
+          <input
+            type="text"
+            name="title"
+            value={note.title}
+            onChange={handleInputChange}
+          />
+        </p>
+        <br />
+        <textarea
+          rows={5}
+          cols={40}
+          name="body"
+          value={note.body}
+          onChange={handleInputChange}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
+};
 
 export default NoteEdit;
