4d31620718ba89eabc36bdd6e5679dea3a5ff107
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-14 01:05:13 +0300

    🎉 feat(api.ts): Add enum for NoteAPI endpoints This commit adds an enum for NoteAPI endpoints to improve code readability and maintainability. The enum contains all the endpoints used in the API, and each endpoint is accessed using the enum instead of hardcoding the endpoint URL. This change also removes the conditional base URL, as it is no longer needed with the new enum.
    Changes made:
    - Added an enum for NoteAPI endpoints.
    - Replaced hardcoded endpoint URLs with the enum.
    - Removed the conditional base URL.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,33 +1,44 @@
 import axios from 'axios';
 import type { INote } from '../types';
 
-const baseURL = import.meta.env.DEV ? 'http://localhost:3000' : '';
+const baseURL = '';
 
 const api = axios.create({ baseURL });
 
+enum NoteAPI {
+  GET_NOTES = '/notes',
+  SAVE_NOTE = '/notes/save/',
+  UPDATE_NOTE = '/notes/save/:key',
+  READ_NOTE = '/notes/view?key=:key',
+  DELETE_NOTE = '/notes/destroy',
+}
+
 export const getNotes = async () => {
-  let response = await api.get<{ notes: INote[] }>('/notes');
+  let response = await api.get<{ notes: INote[] }>(NoteAPI.GET_NOTES);
   return response.data;
 };
 
 export const saveNote = async (note: INote) => {
-  let response = await api.post<INote>('/notes/save/', note);
+  let response = await api.post<INote>(NoteAPI.SAVE_NOTE, note);
   return response.data;
 };
 
 export const updateNote = async (data: INote) => {
   const { key } = data;
-  let response = await api.put<INote>(`/notes/save/${key}`, data);
+  let response = await api.put<INote>(
+    NoteAPI.UPDATE_NOTE.replace(':key', key),
+    data
+  );
   return response.data;
 };
 
 export const readNote = async (key: string) => {
-  let response = await api.get<INote>(`/notes/view?key=${key}`);
+  let response = await api.get<INote>(NoteAPI.READ_NOTE.replace(':key', key));
   return response.data;
 };
 
 export const deleteNote = async (key: string) => {
-  const response = await api.delete('/notes/destroy', {
+  const response = await api.delete(NoteAPI.DELETE_NOTE, {
     params: {
       key,
     },
