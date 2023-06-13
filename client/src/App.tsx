8293be13d6858985a9d2ef7e20f042eff81740e6
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-13 20:30:07 +0300

    🎉 feat(App.tsx): Implement routing for different pages In order to enable navigation between different pages in the application, routing functionality has been implemented using React Router. Changes made: - Imported necessary components for different pages (Index, NoteEdit, NoteView, NoteAdd, NoteDestroy). - Added route definitions for each page using `<Route>` component from React Router. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>
    ✨ feat(index.css): Add basic styling for the application
    To improve the visual appearance of the application, basic styling has been added to the index.css file.
    Changes made:
    - Applied padding and font styling to the body.
    - Added a header with a background color and padding.
    - Adjusted the color of anchor links.
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Index from './pages/Index';
+import NoteEdit from './pages/NoteEdit';
+import NoteView from './pages/NoteView';
+import NoteAdd from './pages/NoteAdd';
+import NoteDestroy from './pages/NoteDestroy';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/notes/add" element={<NoteAdd />}></Route>
+        <Route path="/notes/edit" element={<NoteEdit />}></Route>
+        <Route path="/notes/view" element={<NoteView />}></Route>
+        <Route path="/notes/destroy" element={<NoteDestroy />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
