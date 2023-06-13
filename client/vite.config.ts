8c714b3594e443c9720a46b52c73deaf9d4209a4
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-14 01:05:44 +0300

    🎉 feat(client): Add proxy configuration for notes API To enable the client to communicate with the notes API, a proxy configuration was added to the Vite configuration file. The `/notes` endpoint is now proxied to `http://localhost:3000/`. Changes made: - A `server` object was added to the Vite configuration file. - A `proxy` object was added to the `server` object to map the `/notes` endpoint to `http://localhost:3000/`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/notes': 'http://localhost:3000/',
+    },
+  },
   plugins: [react()],
   build: {
     outDir: 'build',
