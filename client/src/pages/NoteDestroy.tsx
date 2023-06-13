f0a8b1ced81a24f52c34d6a95f0a78e1f347190d
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-13 20:32:29 +0300

    🎉 feat(NoteDestroy.tsx): Add note deletion functionality
    In this commit, the NoteDestroy component was enhanced to include the ability to delete a note. The following changes were made:
    
    - The React import statement was updated to include the `useEffect` and `useState` hooks.
    - The Props interface was removed since it was not being used.
    - The react-router-dom imports for `Link`, `useNavigate`, and `useSearchParams` were added.
    - The deleteNote and readNote functions from the api module were imported.
    - The NoteDestroy component now uses the `useState` hook to manage the state of the note being deleted.
    - The `useEffect` hook was added to fetch the note based on the query parameter 'key' from the URL and update the note state accordingly.
    - The handleSubmit function was implemented to handle the form submission. It prevents the default form submission behavior, deletes the note using the deleteNote function, and navigates back to the home page using the `navigate` function.
    - The form element was updated with an onSubmit event handler and a hidden input field to store the note key.
    - The 'DELETE' button was changed to a regular submit button.
    - The 'Cancel' link was updated to use the `Link` component from react-router-dom.
    
    These changes enable users to delete a note and handle the form submission correctly.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- client/src/pages/NoteDestroy.tsx
+++ client/src/pages/NoteDestroy.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { INote } from '../types';
-interface Props {
-  note?: INote;
-}
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import { deleteNote, readNote } from '../api/api';
+interface Props {}
 
-const NoteDestroy: React.FC<Props> = ({ note }) => (
-  <form method="POST" action="/notes/destroy/confirm">
-    <input type="hidden" name="notekey" value={note ? note.key : ''} />
-    <p>Delete {note && note.title}?</p>
-    <br />
-    <input type="submit" value="DELETE" />
-    <a href={`/notes/view?key=${note && note.key}`}>Cancel</a>
-  </form>
-);
+const NoteDestroy: React.FC<Props> = () => {
+  const [note, setNote] = useState<INote>({
+    key: '',
+    title: '',
+    body: '',
+  });
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const key = searchParams.get('key');
+
+    const getNote = async () => {
+      if (key) {
+        const note = await readNote(key);
+        setNote(note);
+      }
+    };
+
+    getNote();
+  }, []);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { key } = note;
+    await deleteNote(key);
+    navigate('/');
+  };
+  return (
+    <form method="POST" onSubmit={handleSubmit}>
+      <input type="hidden" value={note.key} />
+      <p>Delete {note && note.title}?</p>
+      <br />
+      <input type="submit" />
+      <Link to={`/notes/view?key=${note.key}`}>Cancel</Link>
+    </form>
+  );
+};
 
 export default NoteDestroy;
