b4f88f6200ecb33c1bee64bedf2ec6c5581de1f3
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-13 20:30:57 +0300

    🔧 refactor(Index.tsx): Refactor Index page component to fetch notes from API 🐛 fix(Index.tsx): Fix missing import and incorrect props in Index page component
    The `Index` page component has been refactored to use the `useEffect` hook and `useState` hook to fetch notes from the API. The `getNotes` function from the `api` module is imported to handle the API request. Additionally, the `Header` component is imported and rendered to display a title for the page.
    
    Changes made:
    - Added `useEffect` and `useState` hooks to fetch and store the notes retrieved from the API.
    - Imported and rendered the `Header` component.
    - Imported the `getNotes` function from the `api` module.
    - Imported the `Link` component from `react-router-dom` for routing.
    - Removed unused `noteList` prop from the component definition.
    - Updated the rendering of notes to use the `Link` component for navigation.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { INote } from '../types';
+import Header from './Header';
+import { getNotes } from '../api/api';
+import { Link } from 'react-router-dom';
 
-interface Props {
-  noteList: INote[];
-}
+interface Props {}
 
-const Index: React.FC<Props> = ({ noteList }) => (
-  <ul>
-    {noteList.map(note => (
-      <li key={note.key}>
-        {note.key}: <a href={`/notes/view?key=${note.key}`}>{note.title}</a>
-      </li>
-    ))}
-  </ul>
-);
+const Index: React.FC<Props> = () => {
+  const [notes, setNotes] = useState<INote[]>([]);
+
+  useEffect(() => {
+    getNotes().then(({ notes }) => {
+      setNotes(notes);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header title="Notes" />
+      <ul>
+        {notes.map(note => (
+          <li key={note.key}>
+            {note.key}:{' '}
+            <Link to={`/notes/view?key=${note.key}`}>{note.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default Index;
