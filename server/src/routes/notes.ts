ba36536fc1997e9bc33c6b94cbf7aed19d947788
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-14 01:04:42 +0300

    🎉 feat(server): Add NoteAPI enum to notes route This commit adds a `NoteAPI` enum to the notes route to improve readability and maintainability of the code. The enum contains the API endpoints for getting, saving, updating, reading, and deleting notes. The endpoints are now accessed using the enum instead of hardcoding the string values.
    Changes made:
    - Added `NoteAPI` enum to the notes route.
    - Updated the API endpoints to use the `NoteAPI` enum instead of hardcoded string values.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,22 +1,34 @@
 import express from 'express';
 import { NotesStore as notes } from '../app.js';
 import { INote } from '../models/Notes.js';
+
+enum NoteAPI {
+  GET_NOTES = '/',
+  SAVE_NOTE = '/save',
+  UPDATE_NOTE = '/save/:key',
+  READ_NOTE = '/view',
+  DELETE_NOTE = '/destroy',
+}
+
 export const router = express.Router();
 
-router.get<unknown, { notes: INote[] }>('/', async (req, res, next) => {
-  try {
-    const keyList = await notes.keyList();
-    const keyPromises = keyList.map(key => {
-      return notes.read(key);
-    });
-    res.json({ notes: await Promise.all(keyPromises) });
-  } catch (err) {
-    next(err);
+router.get<unknown, { notes: INote[] }>(
+  NoteAPI.GET_NOTES,
+  async (req, res, next) => {
+    try {
+      const keyList = await notes.keyList();
+      const keyPromises = keyList.map(key => {
+        return notes.read(key);
+      });
+      res.json({ notes: await Promise.all(keyPromises) });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // Save Note
-router.post<{}, INote, INote>('/save', async (req, res, next) => {
+router.post<{}, INote, INote>(NoteAPI.SAVE_NOTE, async (req, res, next) => {
   const { key, title, body } = req.body;
   try {
     const note = await notes.create(key, title, body);
@@ -27,31 +39,37 @@ router.post<{}, INote, INote>('/save', async (req, res, next) => {
 });
 
 // Update Note
-router.put<unknown, INote, INote>('/save/:key', async (req, res, next) => {
-  const { key, title, body } = req.body;
-  try {
-    const note = await notes.update(key, title, body);
-    res.json(note);
-  } catch (err) {
-    next(err);
+router.put<unknown, INote, INote>(
+  NoteAPI.UPDATE_NOTE,
+  async (req, res, next) => {
+    const { key, title, body } = req.body;
+    try {
+      const note = await notes.update(key, title, body);
+      res.json(note);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // Read Note
-router.get<{}, INote, {}, { key: string }>('/view', async (req, res, next) => {
-  console.log(req.query.key);
+router.get<{}, INote, {}, { key: string }>(
+  NoteAPI.READ_NOTE,
+  async (req, res, next) => {
+    console.log(req.query.key);
 
-  try {
-    let note = await notes.read(req.query.key);
-    res.json(note);
-  } catch (err) {
-    next(err);
+    try {
+      let note = await notes.read(req.query.key);
+      res.json(note);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // Delete Note
 router.delete<{}, INote, {}, { key: string | undefined }>(
-  '/destroy',
+  NoteAPI.DELETE_NOTE,
   async (req, res, next) => {
     try {
       const key = req.query.key as string | undefined;
